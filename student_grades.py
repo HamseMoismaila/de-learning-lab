9290b4758199aea7cb65565150f9a4d59c4ecc5e
Hamse Mohamed <Hamze2778@gmail.com>
2026-04-28 23:57:50 +0800

    the value

---
+++ student_grades.py
@@ -0,0 +1,43 @@
+
+students = {
+    "Ali": 78,
+    "Sara": 92,
+    "John": 45,
+    "Aisha": 66,
+    "Mike": 30,
+    "Lina": 88,
+    "David": 55,
+    "Zara": 49,
+    "Omar": 73,
+    "Noah": 61
+}
+
+
+
+# def class_average(students) :
+#     if not students: 
+#         raise ValueError("No ostudent provided")
+    
+#     return sum(students.values()) / len(students)
+
+
+# print(class_average(students))
+
+
+
+
+
+student  = {
+    "name" : "Alice", 
+    "age" : 20, 
+    "course" : "Computer Science"
+}
+
+
+student.pop("name")
+
+del student["age"]
+
+print(student)
+
+
