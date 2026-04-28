5621b6ae9aa4e41c2188ced45e34233de552279a
Hamse Mohamed <Hamze2778@gmail.com>
2026-04-29 01:23:28 +0800

    list and controlling

--- student_grades.py
+++ student_grades.py
@@ -14,30 +14,100 @@ students = {
 
 
 
-# def class_average(students) :
-#     if not students: 
-#         raise ValueError("No ostudent provided")
+def class_average(students) :
+    if not students: 
+        raise ValueError("No ostudent provided")
     
-#     return sum(students.values()) / len(students)
+    return sum(students.values()) / len(students)
 
 
-# print(class_average(students))
+def top_3(students):
+    sorted_student = sorted(students.items(), key=lambda x: x[1], reverse=True)
+    return sorted_student[:3]
+
+
+def fail_list(students):
+    return [name for  name, score in students.items() if score < 50]
+
+
+def grade_distribution(students):
+    distribution = {"A": 0, "B": 0, "C": 0, "D": 0, "F": 0}
+
+    for score in students.values():
+        if score >= 80:
+            distribution["A"] += 1
+        elif score >= 70:
+            distribution["B"] += 1
+        elif score >= 60:
+            distribution["C"] += 1
+        elif score >= 50:
+            distribution["D"] += 1
+        else:
+            distribution["F"] += 1
+
+    return distribution
+
+
+def normalize_scores(students):
+    max_score = max(students.values())
+    return {name: round(score / max_score * 100, 2) for name, score in students.items()}
 
 
 
 
 
-student  = {
-    "name" : "Alice", 
-    "age" : 20, 
-    "course" : "Computer Science"
-}
+data =  [10, 20, 30 ]
+
+max_val = max(data)
+
+normalized = [ round(x / max_val, 2) for x in data]
 
 
-student.pop("name")
 
-del student["age"]
+print(normalized)
 
-print(student)
+
+
+
+
+
+
+
+
+# goals = {
+#     "Ronaldo" : 234, 
+#     "Messi" : 700, 
+#     "Lewandowski" : 590
+# }
+
+
+
+
+
+# def all_time_goal_scorer(goals):
+#     sorted_player = sorted(goals.items(), key= lambda x: x[1], reverse=True )
+#     return sorted_player[:3]
+
+# print(all_time_goal_scorer(goals))
+
+
+# def all_time_goal_scorer(goals):
+#     top = []
+
+#     for player, goals in goals.items():
+#         top.append((player, goals))
+#         top.sort(key=lambda x: x[1], reverse=True)
+
+#     return top[:3]
+
+# print(all_time_goal_scorer(goals))
+
+
+
+
+
+
+
+
 
 
